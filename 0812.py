513064b672ec7ecf7e687b1ac9d6f76aa27a8a64
Chen <1455075085@qq.com>
2019-08-12 11:22:12 +0800

    set method

--- 0812.py
+++ 0812.py
@@ -50,3 +50,22 @@ d = {
 print d['age']
 print d.get('Age') # 使用get方法，在key不存在的时候，返回None
 print len(d)
+# dict特点：查找速度快，但是占用内存大，会浪费很多内容，但是list正好相反，占用内存小，但是查找速度慢
+
+# 更新dict
+d['Paul'] = 97 # 如果建已存在，则赋值会用新的value替换掉原来的value
+print d
+
+# 遍历dict
+for key in d:
+	print key
+# 遍历键值对
+for key in d:
+	print key, d[key]
+# set 方法
+s = set(['A', 'B', 'C', 'D'])
+print s
+print 's的长度是', len(s)
+
+# 使用in来判断某一元素是不是在set中
+print 'a' in s
